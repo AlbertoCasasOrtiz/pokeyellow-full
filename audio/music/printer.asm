884f612104f32a0f28dcf923bc3d4288116d762d
Alberto <albertocasasortiz@gmail.com>
2022-07-04 03:10:35 +0200

    Update printer.asm

--- audio/music/printer.asm
+++ audio/music/printer.asm
@@ -2,34 +2,6 @@ Music_GBPrinter_Ch3::
 	tempo 256
 	note_type 12, 1, 0
 	octave 4
-	note B_, 6
-	octave 5
-	note C#, 1
-	rest 1
-	octave 4
-	note B_, 6
-	octave 5
-	note C#, 1
-	rest 1
-	octave 4
-	note B_, 1
-	rest 1
-	octave 5
-	note E_, 1
-	rest 1
-	note D#, 1
-	rest 1
-	note C#, 1
-	rest 1
-	octave 4
-	note B_, 1
-	rest 1
-	note A_, 1
-	rest 1
-	note G#, 1
-	rest 1
-	note F#, 1
-	rest 1
 .loop
 	note D_, 1
 	note E_, 2
@@ -37,40 +9,48 @@ Music_GBPrinter_Ch3::
 	note E_, 2
 	note E_, 1
 	note E_, 1
-	note E_, 2
+	note E_, 1
 	note D_, 1
 	note C_, 1
 	note C_, 3
-	rest 1
-	note E_, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note E_, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note E_, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note F#, 1
-	rest 1
-	note A_, 1
-	rest 1
-	note F#, 1
-	rest 1
-	note A_, 1
-	rest 1
-	note F#, 1
-	rest 1
-	octave 5
-	note C#, 1
+
 	note C_, 1
-	note C#, 1
-	rest 1
-	octave 4
-	note F#, 1
+        note C_, 1
+	note D_, 1
+        note D_, 3
+        note C_, 1
+        note B_, 1
+        note A_, 4
+
+	note A_, 1
+	note E_, 1
+	note E_, 1
+	note E_, 1
+	note E_, 2
+	note G_, 1
+	note G_, 2
+	note E_, 4
+	
+	note C_, 1
+        note C_, 1
+	note D_, 1
+        note E_, 3
+        note D_, 1
+        note C_, 1
+        note D_, 4
+
+	
+
+	
+
+        note D_, 3
+
+        note C_, 1
+
+        note B_, 1
+
+        note A_, 4
+	
 	rest 1
 	note E_, 1
 	rest 1
